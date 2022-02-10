2bd9fe043be347e494e0fc3b28a94a0a2b2bbba4
aakanksha thakre <aakanksha.thakre@equinox.com>
2022-02-10 14:49:47 +0530

    bootstrap css functioning

--- container/src/components/home/index.jsx
+++ container/src/components/home/index.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
 
 import styles from './index.module.scss';
+import globalStyles from '../../../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 const Home = () => {
-  return <button className={styles.error_btn}>Success</button>;
+  return (
+    <button className={`${styles.error_btn} ${globalStyles['btn-primary']} row `}>
+      Success
+    </button>
+  );
 };
 export default Home;
